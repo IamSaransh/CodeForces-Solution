fee43ef0ba1b0bf52eef02638a20bf312db14a1e
Saransh <saranshvashistha@gmail.com>
2022-05-03 12:03:47 +0530

    repo created

--- src/main/java/com/codeforces/contest786/B.java
+++ src/main/java/com/codeforces/contest786/B.java
@@ -1,37 +1,27 @@
 package com.codeforces.contest786;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 
 public class B {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int TT = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader bfn = new BufferedReader(new InputStreamReader(System.in));
+        int TT = Integer.parseInt(bfn.readLine());
         for(int tt=0; tt<TT;tt++){
-            int n;
-            n=sc.nextInt();
-            int[] arr = new int[n];
-            for(int i=0; i<n; i++)
-            {
-                arr[i]=sc.nextInt();
-            }
-            Map<Integer, Integer> map = new HashMap<>();
-            for(int i=0; i<n;i++ ){
-                if(map.containsKey(arr[i])){
-                    map.put(arr[i], map.get(arr[i])+1);
-                }
-                else{
-                    map.put(arr[i],1);
-                }
-            }
-            int ans = -1;
-            for(int val: map.keySet()){
-                if(map.get(val)>=3){
-                    ans = val;
-                }
-            }
+            String s = bfn.readLine();
+            char l1 = s.charAt(0);
+            char l2 = s.charAt(1);
+
+            //there will be 25 words per one cycle since one common pair will be ignored
+            int ans = 0;
+            ans = 25* (l1-'a');
+            ans += l2-'a' +1;
+            if(l2>l1)
+                ans--;
             System.out.println(ans);
+
 
         }
     }
