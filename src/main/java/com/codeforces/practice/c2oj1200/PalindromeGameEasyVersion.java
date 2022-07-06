8fbb50917e426f3039b0369d7b3ec5d10d9da3ce
Saransh <saranshvashistha@gmail.com>
2022-07-07 00:07:18 +0530

    c2oj <1200 started

---
+++ src/main/java/com/codeforces/practice/c2oj1200/PalindromeGameEasyVersion.java
@@ -0,0 +1,149 @@
+package com.codeforces.practice.c2oj1200;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.*;
+/*
+1527B1 - Palindrome Game (easy version)
+If the count of zeros in the string s is even then Bob always win
+
+Proof
+
+Bob can restrict Alice from performing operation 2 by making string s palindrome (if Alice changes s[i] to '1' then Bob will change s[n−i+1] to '1'). However, when the last '0' is remaining, Bob will reverse the string, eventually forcing Alice to perform the operation 1. This way Alice will spend 2 dollars more than Bob resulting in Bob's win.
+If the count of zeros in the string s is odd then Alice always win except for the case of one '0'
+Proof
+
+Alice will change s[n/2] from '0' to '1' and play with the same strategy as Bob did in the above case. This way Bob will spend 1 dollar more than Alice resulting in Alice's win.
+ */
+public class PalindromeGameEasyVersion {
+
+
+    public static void main(String[] args) {
+        FastScanner fs = new FastScanner();
+        PrintWriter out = new PrintWriter(System.out);
+        int T = fs.nextInt();
+        while(T-->0)
+        {
+            int n = fs.nextInt();
+            String s = fs.next();
+            int zeroCount = 0;
+            for(char c: s.toCharArray()){
+                if(c == '0')
+                    zeroCount++;
+            }
+            if(zeroCount %2==0 || zeroCount==1){
+                System.out.println("BOB");
+            }
+            else
+                System.out.println("ALICE");
+        }
+
+        out.close();
+    }
+
+
+    static final Random random = new Random();
+    static final int mod = 1_000_000_007;
+
+    static void ruffleSort(int[] a) {
+        int n = a.length;//shuffle, then sort
+        for (int i = 0; i < n; i++) {
+            int oi = random.nextInt(n), temp = a[oi];
+            a[oi] = a[i];
+            a[i] = temp;
+        }
+        Arrays.sort(a);
+    }
+
+    boolean isSorted(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    static long add(long a, long b) {
+        return (a + b) % mod;
+    }
+
+    static long sub(long a, long b) {
+        return ((a - b) % mod + mod) % mod;
+    }
+
+    static long mul(long a, long b) {
+        return (a * b) % mod;
+    }
+
+    static long exp(long base, long exp) {
+        if (exp == 0) return 1;
+        long half = exp(base, exp / 2);
+        if (exp % 2 == 0) return mul(half, half);
+        return mul(half, mul(half, base));
+    }
+
+    static long[] factorials = new long[2_000_001];
+    static long[] invFactorials = new long[2_000_001];
+
+    static void precompFacts() {
+        factorials[0] = invFactorials[0] = 1;
+        for (int i = 1; i < factorials.length; i++) factorials[i] = mul(factorials[i - 1], i);
+        invFactorials[factorials.length - 1] = exp(factorials[factorials.length - 1], mod - 2);
+        for (int i = invFactorials.length - 2; i >= 0; i--)
+            invFactorials[i] = mul(invFactorials[i + 1], i + 1);
+    }
+
+    static long nCk(int n, int k) {
+        return mul(factorials[n], mul(invFactorials[k], invFactorials[n - k]));
+    }
+
+    static void sort(int[] a) {
+        ArrayList<Integer> l = new ArrayList<>();
+        for (int i : a) l.add(i);
+        Collections.sort(l);
+        for (int i = 0; i < a.length; i++) a[i] = l.get(i);
+    }
+
+    static int gcd(int a, int b) {
+        while (b != 0) {
+            int t = a;
+            a = b;
+            b = t % b;
+        }
+        return a;
+    }
+
+    private static class FastScanner {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer("");
+
+        String next() {
+            while (!st.hasMoreTokens())
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        int[] readArray(int n) {
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) a[i] = nextInt();
+            return a;
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+    }
+
+
+}
