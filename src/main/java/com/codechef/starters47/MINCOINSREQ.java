d7f959ac78f79ea67069163932bd223c220fc818
saransh <saranshvashistha@gmail.com>
2022-07-14 02:47:16 +0530

    codeChef cry

--- src/main/java/com/codechef/starters47/MINCOINSREQ.java
+++ src/main/java/com/codechef/starters47/MINCOINSREQ.java
@@ -1,4 +1,4 @@
-package com.algorithms;
+package com.codechef.starters47;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -6,61 +6,26 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.util.*;
 
-public class QuickSort {
+public class MINCOINSREQ {
 
     public static void main(String[] args) {
-       int[] arr = {9,8,7,6,5,4,3,2,1,1,1,};
-       quickSort(arr, 0, arr.length-1);
-       printArray(arr);
-    }
+        FastScanner fs = new FastScanner();
+        PrintWriter out = new PrintWriter(System.out);
+        int TT = fs.nextInt();
+        //int TT = 1;
+        while (TT-- > 0) {
+            //Write your code here
+            int n = fs.nextInt();
+            System.out.println(n%10);
 
-    static final void printArray(int[] arr){
-        for(int x: arr)
-            System.out.print(x + " ");
-    }
-
-    private static void quickSort(int[] arr, int s, int e) {
-        //@Base Case
-        if(s>=e)
-            return;
-
-        int p = partition(arr, s, e);
-        quickSort(arr, s, p-1 );
-        quickSort(arr, p+1, e );
-    }
-
-    private static int partition(int[] arr, int s, int e) {
-        int pivot = arr[s];
-        int count = 0;
-        for(int i = s+1; i<arr.length; i++)
-        {
-            if(arr[i] < pivot)
-                count++;
         }
-        // place the pivot element atr its right place
-        int pivotIndex = s + count;
-        swap(arr, s, pivotIndex);
 
-        int i = s; int j = e;
-        while(i<pivotIndex && j> pivotIndex)
-        {
-            while(arr[i]< pivot)
-                i++;
-            while(arr[j]>pivot)
-                j++;
-            //ab jaha ruke hai vaha pivot change krna hai!
-            if(i<pivotIndex && j> pivotIndex)
-                swap(arr, i++, j--);
-        }
-        return pivotIndex;
+        out.close();
     }
 
-    @SuppressWarnings("unused")
     static final Random random = new Random();
-    @SuppressWarnings("unused")
     static final int mod = 1_000_000_007;
 
-    @SuppressWarnings("unused")
     static void ruffleSort(int[] a) {
         int n = a.length;//shuffle, then sort
         for (int i = 0; i < n; i++) {
@@ -70,7 +35,7 @@ public class QuickSort {
         }
         Arrays.sort(a);
     }
-    @SuppressWarnings("unused")
+
     static boolean isSorted(int[] arr) {
         for (int i = 0; i < arr.length - 1; i++) {
             if (arr[i] > arr[i + 1]) {
@@ -80,21 +45,19 @@ public class QuickSort {
         return true;
     }
 
-    @SuppressWarnings("unused")
+
     static long add(long a, long b) {
         return (a + b) % mod;
     }
 
-    @SuppressWarnings("unused")
     static long sub(long a, long b) {
         return ((a - b) % mod + mod) % mod;
     }
 
-    @SuppressWarnings("unused")
     static long mul(long a, long b) {
         return (a * b) % mod;
     }
-    @SuppressWarnings("unused")
+
     static long exp(long base, long exp) {
         if (exp == 0) return 1;
         long half = exp(base, exp / 2);
@@ -105,7 +68,6 @@ public class QuickSort {
     static long[] factorials = new long[2_000_001];
     static long[] invFactorials = new long[2_000_001];
 
-    @SuppressWarnings("unused")
     static void precompFacts() {
         factorials[0] = invFactorials[0] = 1;
         for (int i = 1; i < factorials.length; i++) factorials[i] = mul(factorials[i - 1], i);
@@ -113,12 +75,11 @@ public class QuickSort {
         for (int i = invFactorials.length - 2; i >= 0; i--)
             invFactorials[i] = mul(invFactorials[i + 1], i + 1);
     }
-    @SuppressWarnings("unused")
+
     static long nCk(int n, int k) {
         return mul(factorials[n], mul(invFactorials[k], invFactorials[n - k]));
     }
 
-    @SuppressWarnings("unused")
     static void sort(int[] a) {
         ArrayList<Integer> l = new ArrayList<>();
         for (int i : a) l.add(i);
@@ -126,7 +87,6 @@ public class QuickSort {
         for (int i = 0; i < a.length; i++) a[i] = l.get(i);
     }
 
-    @SuppressWarnings("unused")
     static int gcd(int a, int b) {
         while (b != 0) {
             int t = a;
@@ -140,7 +100,6 @@ public class QuickSort {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer("");
 
-        @SuppressWarnings("unused")
         String next() {
             while (!st.hasMoreTokens())
                 try {
@@ -150,7 +109,7 @@ public class QuickSort {
                 }
             return st.nextToken();
         }
-        @SuppressWarnings("unused")
+
         int nextInt() {
             return Integer.parseInt(next());
         }
@@ -160,12 +119,12 @@ public class QuickSort {
             for (int i = 0; i < n; i++) a[i] = nextInt();
             return a;
         }
-        @SuppressWarnings("unused")
+
         long nextLong() {
             return Long.parseLong(next());
         }
     }
-    @SuppressWarnings("unused")
+
     static void shuffleArray(long[] arr) {
         int n = arr.length;
         Random rnd = new Random();
@@ -176,11 +135,11 @@ public class QuickSort {
             arr[randomPos] = tmp;
         }
     }
-    @SuppressWarnings("unused")
+
     public static void swap(int[] arr, int i, int j) {
         arr[i] = (arr[i] + arr[j]) - (arr[j] = arr[i]);
     }
-    @SuppressWarnings("unused")
+
     class Pair<S extends Comparable<S>, T extends Comparable<T>> implements Comparable<Pair<S, T>> {
         S first;
         T second;
--- src/main/java/com/leetcode/practice/Solution.java
+++ src/main/java/com/leetcode/practice/Solution.java
@@ -1,132 +1,50 @@
 package com.leetcode.practice;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
+import java.util.List;
 
 class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> currAns = new ArrayList<>();
+        int index= 0;
+        //helper function call to fill ans with all subsets
+        helper(nums, index, ans,currAns );
+        //return the result
+        return ans;
+    }
 
-    static void mergeSort(int [] arr, int s, int e)
-    {
-        if(s>=e)
+    static  void helper(int[] nums,  int index,
+                              List<List<Integer>> ans, List<Integer> currAns){
+        //@Base case
+        if(index >= nums.length)
+        {
+            ans.add(currAns);
             return;
-        int mid = (s+e)/2;
-
-        mergeSort(arr, s, mid);
-        mergeSort(arr, mid+1, e);
-        merge(arr, s, e);
-
-
-    }
-    static void merge2(int arr[], int l, int m, int r)
-    {
-        // Find sizes of two subarrays to be merged
-        int n1 = m - l + 1;
-        int n2 = r - m;
-        int x = l;
-        /* Create temp arrays */
-        int L[] = new int[n1];
-        int R[] = new int[n2];
-
-        /*Copy data to temp arrays*/
-        for (int i = 0; i < n1; ++i)
-            L[i] = arr[x++];
-        x = m+1;
-        for (int j = 0; j < n2; ++j)
-            R[j] = arr[x++];
-
-        /* Merge the temp arrays */
-
-        // Initial indexes of first and second subarrays
-        int i = 0, j = 0;
-
-        // Initial index of merged subarray array
-        int k = l;
-        while (i < n1 && j < n2) {
-            if (L[i] <= R[j]) {
-                arr[k] = L[i];
-                i++;
-            }
-            else {
-                arr[k] = R[j];
-                j++;
-            }
-            k++;
         }
-
-        /* Copy remaining elements of L[] if any */
-        while (i < n1) {
-            arr[k] = L[i];
-            i++;
-            k++;
-        }
-
-        /* Copy remaining elements of R[] if any */
-        while (j < n2) {
-            arr[k] = R[j];
-            j++;
-            k++;
-        }
-    }
-
-
-    private static void merge(int[] arr, int s, int e) {
-        int mid  = (s+e)/2;
-
-        int len1 = mid-s+1;
-        int len2 = e-mid;
-        int[] first = new int[len1];
-        int[] second = new int[len2];
-        //copy values
-        int k =s;
-        for(int i=0; i<len1;i++){
-            first[i] = arr[k++];
-        }
-        k = mid+1;
-        for(int i=0; i<len2;i++){
-            second[i] = arr[k++];
-        }
-
-        //merge two sorted arrays logic here
-        int index1 = 0;
-        int index2 =0;
-        k = s;
-
-        while(index1< len1 && index2< len2){
-            if (first[index1] < second[index2])
-            {
-                arr[k++] = first[index1++];
-            }
-            else{
-                arr[k++] = second[index2++];
-            }
-        }
-        while(index1<len1){
-            arr[k++] = first[index1++];
-        }
-        while(index2< len2){
-            arr[k++] = second[index2++];
-        }
-
-    }
-    private static void check(String s) {
-        for(int i=1; i<s.length()-1;i++){
-            String s1 = s.substring(0, i+1);
-            String s2 = s.substring(i+1);
-
-        }
-
+        System.out.println(currAns);
+        //else make include and non include call for that element at that index;
+        //non inc
+        helper(nums, index+1, ans, currAns);
+        //inc call
+        currAns.add(nums[index]); //add element and make recursive call
+        helper(nums, index+1, ans, currAns );
+        //backtrack
+        int lastIndex = currAns.size() - 1;
+        currAns.remove(lastIndex);
     }
 
     public static void main(String[] args) {
-        int [] arr = {1,2,3,4,100,6,6,6,6,6,6,7,8};
-        mergeSort(arr, 0, arr.length-1);
-        for(int a: arr){
-            System.out.print(a + " ");
-        }
+//        Solution solution = new Solution();
+//        int[] nums = {1,2,3};
+//        List<List<Integer>> ans = solution.subsets(nums);
+//        System.out.println(ans);
+//        ;
+
 
 
 
     }
 }
-
-
