3d46547c5399ebeeca4bf00051496065e6eab21b
Saransh <saranshvashistha@gmail.com>
2022-07-24 19:07:22 +0530

    leetcode contest bkp

--- src/main/java/com/leetcode/practice/FoodRatings.java
+++ src/main/java/com/leetcode/practice/FoodRatings.java
@@ -1,60 +1,74 @@
-package com.leetcode.practice;
+ package com.leetcode.practice;
 
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.*;
 
 class FoodRatings {
-    Map<String, TreeMap<String, Integer>> map;
-    Map<String, String> foodCuisineMap;
+    Map<String, PriorityQueue<Food>> map;
+    Map<String, String> menu;
 
     public FoodRatings(String[] foods, String[] cuisines, int[] ratings) {
-        map = new HashMap< String, TreeMap<String, Integer>>();
-        foodCuisineMap = new HashMap<>();
-        for(String cuisine: cuisines){
-            if(!map.containsKey(cuisine))
+        map = new HashMap<>();
+        menu = new HashMap<>();
+        for(int i=0; i<foods.length; i++){
+            if(!map.containsKey(cuisines[i]))
             {
-//                TreeMap<String, Integer> mapCusine = new TreeMap<>(Collections.reverseOrder());
-                TreeMap<String, Integer> mapCusine = new TreeMap<>();
+                PriorityQueue<Food> queue = new PriorityQueue<Food>(new Comparator<Food>() {
+                    public int compare(Food n1, Food n2) {
+                        if(n1.foodRating == n2.foodRating)
+//                            return String.CASE_INSENSITIVE_ORDER.compare(n1.foodName, n2.foodName);
+                            return  n1.foodName.compareTo(n2.foodName);
+                        else
 
-                map.put(cuisine, mapCusine);
+                            return Integer.compare(n2.foodRating, n1.foodRating);
+                    } });
+                map.put(cuisines[i], queue);
             }
+            Food curr = new Food(foods[i], ratings[i]);
+            map.get(cuisines[i]).add(curr);
+            menu.put(foods[i], cuisines[i]);
 
         }
-        //
-        int n = foods.length;
-        for(int i=0;i<n;i++)
-        {
-            map.get(cuisines[i]).put(foods[i],ratings[i] );
-            //aslso add value to foodCusineMap
-            foodCuisineMap.put(foods[i],cuisines[i] );
 
-        }
-        System.out.println("INITIAL");
-        System.out.println(map);
-        System.out.println(foodCuisineMap);
     }
 
     public void changeRating(String food, int newRating) {
-        String currCusine = foodCuisineMap.get(food);
-        map.get(currCusine).put(food, newRating);   ///food is unique value so will overwrite bgy default
-        System.out.println("CHANGED");
-        System.out.println(map);
+        String currCuisine = menu.get(food);
+        Food currFood = new Food(food, 12);
+        map.get(currCuisine).remove(currFood);
+        currFood.foodRating = newRating;
+        map.get(currCuisine).add(currFood);
 
     }
 
     public String highestRated(String cuisine) {
-        int maxRating = -1;
-        String ans = "";
-        Map<String, Integer> temp = map.get(cuisine);
-        for(Map.Entry<String, Integer> entry: temp.entrySet()){
-            if(entry.getValue()>maxRating){
-                ans = entry.getKey();
-            }
-        }
-        return ans;
+        return map.get(cuisine).peek().foodName;
     }
+
+
+    static class Food {
+        private String foodName;
+        private int foodRating;
+
+        public Food(String foodName, int foodRating) {
+            this.foodName = foodName;
+            this.foodRating = foodRating;
+        }
+
+         @Override
+         public boolean equals(Object o) {
+             if (this == o) return true;
+             if (o == null || getClass() != o.getClass()) return false;
+             Food food = (Food) o;
+             return foodName.equals(food.foodName);
+         }
+
+         @Override
+         public int hashCode() {
+             return Objects.hash(foodName);
+         }
+    }
+
 }
 
 /**
