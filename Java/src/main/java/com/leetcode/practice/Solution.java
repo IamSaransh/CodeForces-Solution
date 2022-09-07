7c32f8463d691c07c9054c15d37ed8b40ad1965b
Saransh <saranshvashistha@gmail.com>
2022-09-07 15:36:38 +0530

    FEAT: 1200 RESUMED

--- Java/src/main/java/com/leetcode/practice/Solution.java
+++ Java/src/main/java/com/leetcode/practice/Solution.java
@@ -3,43 +3,87 @@ package com.leetcode.practice;
 import java.util.*;
 
 class Solution {
-    public int[] numsSameConsecDiff(int n, int k) {
-        List<Integer> res = new ArrayList<>();
-        String curr = "";
-        //leading value can't be zero
-        for(int i=1;i<10;i++){
-            curr+=i;
-            helper(res, curr, n, k);
-            curr=curr.substring(0, curr.length()-1);
-        }
-        int[] array = new int[res.size()];
-        System.out.println(res);
-        // res.toArray(array); // fill the array
-        return array;
-
-    }
-    void helper(List<Integer> res, String curr,int n, int k){
-        if(curr.length()==n) {
-            res.add(Integer.valueOf(curr));
-            return;
-        }
-        for(int i=0;i<10;i++)
-        {
-            int diff = curr.charAt(curr.length()-1)-'0' - i;
-
-            System.out.println(diff + " " + curr + " " + i);
-            if(Math.abs(diff)==k)
-            {
-                curr+=i;
-                helper(res, curr, n, k);
-                curr=curr.substring(0, curr.length()-1);
-            }
-        }
-
-    }
-
     public static void main(String[] args) {
-        Solution s = new Solution();
-        s.numsSameConsecDiff(3,7);
+        List<Response> result = new ArrayList<>();
+        Response r1 = new Response(11, "AAABBBCCCFF");
+        Response r2 = new Response(11, "ABBBBBCCCFF");
+        Response r3 = new Response(7, "AAAAAAA");
+        Response r4 = new Response(7, "BBBBBBB");
+        result.add(r2);
+        result.add(r1);
+        result.add(r4);
+
+        result.add(r3);
+
+        System.out.println("Result before sorting \n:" + result);
+        Collections.sort(result, Collections.reverseOrder());
+        System.out.println("Result after sorting \n:" + result);
+    }
+
+
+}
+class Response implements  Comparable<Response> {
+    int matchLength;
+    String matchGrade;
+
+    @Override
+    public String toString() {
+        return "Response{" +
+                "matchLength=" + matchLength +
+                ", matchGrade='" + matchGrade + '\'' +
+                '}';
+    }
+
+    public Response(int matchLength, String matchGrade) {
+        this.matchLength = matchLength;
+        this.matchGrade = matchGrade;
+    }
+
+
+    /**
+     * Compares this object with the specified object for order.  Returns a
+     * negative integer, zero, or a positive integer as this object is less
+     * than, equal to, or greater than the specified object.
+     *
+     * <p>The implementor must ensure <tt>sgn(x.compareTo(y)) ==
+     * -sgn(y.compareTo(x))</tt> for all <tt>x</tt> and <tt>y</tt>.  (This
+     * implies that <tt>x.compareTo(y)</tt> must throw an exception iff
+     * <tt>y.compareTo(x)</tt> throws an exception.)
+     *
+     * <p>The implementor must also ensure that the relation is transitive:
+     * <tt>(x.compareTo(y)&gt;0 &amp;&amp; y.compareTo(z)&gt;0)</tt> implies
+     * <tt>x.compareTo(z)&gt;0</tt>.
+     *
+     * <p>Finally, the implementor must ensure that <tt>x.compareTo(y)==0</tt>
+     * implies that <tt>sgn(x.compareTo(z)) == sgn(y.compareTo(z))</tt>, for
+     * all <tt>z</tt>.
+     *
+     * <p>It is strongly recommended, but <i>not</i> strictly required that
+     * <tt>(x.compareTo(y)==0) == (x.equals(y))</tt>.  Generally speaking, any
+     * class that implements the <tt>Comparable</tt> interface and violates
+     * this condition should clearly indicate this fact.  The recommended
+     * language is "Note: this class has a natural ordering that is
+     * inconsistent with equals."
+     *
+     * <p>In the foregoing description, the notation
+     * <tt>sgn(</tt><i>expression</i><tt>)</tt> designates the mathematical
+     * <i>signum</i> function, which is defined to return one of <tt>-1</tt>,
+     * <tt>0</tt>, or <tt>1</tt> according to whether the value of
+     * <i>expression</i> is negative, zero or positive.
+     *
+     * @param o the object to be compared.
+     * @return a negative integer, zero, or a positive integer as this object
+     * is less than, equal to, or greater than the specified object.
+     * @throws NullPointerException if the specified object is null
+     * @throws ClassCastException   if the specified object's type prevents it
+     *                              from being compared to this object.
+     */
+    @Override
+    public int compareTo(Response o) {
+        if(this.matchLength!= o.matchLength)
+            return this.matchLength-o.matchLength;
+        else if(this.matchGrade.compareTo(o.matchGrade)!=0)
+            return -this.matchGrade.compareTo(o.matchGrade);
+        else  return 0;
     }
 }
