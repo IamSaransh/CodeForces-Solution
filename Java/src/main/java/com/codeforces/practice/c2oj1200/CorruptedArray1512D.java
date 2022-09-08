9ee1b4c8eb5f3b209aa6ee3dc9960e467fd3a46d
Saransh <saranshvashistha@gmail.com>
2022-09-08 15:30:53 +0530

    FEAT: 1200 RESUMED

--- Java/src/main/java/com/codeforces/practice/c2oj1200/CorruptedArray1512D.java
+++ Java/src/main/java/com/codeforces/practice/c2oj1200/CorruptedArray1512D.java
@@ -6,69 +6,60 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.util.*;
 
-public class WrongAddition1619C {
+public class CorruptedArray1512D {
 
 
     public static void main(String[] args) {
         FastScanner fs = new FastScanner();
         PrintWriter out = new PrintWriter(System.out);
         int TT = fs.nextInt();
-        /**
-         * IP
-         * 6
-         * 17236 1106911
-         * 1 5
-         * 108 112
-         * 12345 1023412
-         * 1 11
-         * 1 20
-         */
         while (TT-- > 0) {
-            long a, s;
-            a = fs.nextLong();
-            s = fs.nextLong();
-            StringBuilder b = new StringBuilder("");
-            while(s>0){
-                int x = (int) (a % 10);
-                int y = (int) (s % 10);
-                if(x <= y) b.append(y - x);
-                else{
-                    s /= 10;
-                    y += 10 * (s % 10);
-                    if(x < y && y >= 10 && y <= 19) b.append(y - x);
-                    else{
-                        out.println(-1);
-                        return;
+            int n = fs.nextInt();
+            int b[] = fs.readArray(n+2);
+            ruffleSort(b);
+            //now the last element is the sum element;
+            int sum = b[n+1];
+            int indexThatShouldBeRemoved  = -1;
+            int sumTillNPlus1 = 0;
+            for(int i=0;i<n+1;i++) {
+                sumTillNPlus1+=b[i];
+            }
+            for(int i=0;i<n+1;i++){
+                if(sumTillNPlus1-b[i] == sum) {
+                    indexThatShouldBeRemoved = i;
+                    break;
+                }
+            }
+//            if(n==1)
+//            {
+//                if(b[0]==b[2]){
+//                    out.print(b[0]);
+//                }
+//                else if(b[1]==b[2])
+//                    out.println(b[1]);
+//                else out.println(-1);
+//
+//                continue; // go to next testcaswe
+//            }
+            if(indexThatShouldBeRemoved==-1){
+                out.println(-1);
+            }
+            else{
+                for(int i=0;i<n+1;i++){
+                    if(i!=indexThatShouldBeRemoved){
+                        out.print(b[i]);
+                        out.print(" ");
                     }
                 }
-                a /= 10;
-                s /= 10;
-            }
-            if(a>0) out.println(-1);
-            else{
-                while (b.substring(b.length()-1).equalsIgnoreCase("0"))
-                    b= new StringBuilder(b.substring(0, b.length() - 1));
-                String sss = b.toString();
-                for(int i = b.length() - 1; i >= 0; i--) out.print(sss.charAt(i));
                 out.print("\n");
             }
+
+
         }
+
         out.close();
         out.flush();
     }
-    private static Integer getLast1Chars(String str){
-        int len = str.length();
-        String lastXCharr = str.substring(len-1);
-        if(lastXCharr.equalsIgnoreCase("")) return 0;
-        return Integer.valueOf(lastXCharr);
-    }
-
-    private static Integer getLast2Chars(String str){
-        int len = str.length();
-        String lastXCharr = str.substring(len-2);
-        return Integer.valueOf(lastXCharr);
-    }
-
 
 
     static final Random random = new Random();
@@ -120,7 +111,7 @@ public class WrongAddition1619C {
 
     static long[] factorials = new long[2_000_001];
     static long[] invFactorials = new long[2_000_001];
-    @SuppressWarnings("unused")
+
     static void precompFacts() {
         factorials[0] = invFactorials[0] = 1;
         for (int i = 1; i < factorials.length; i++) factorials[i] = mul(factorials[i - 1], i);
